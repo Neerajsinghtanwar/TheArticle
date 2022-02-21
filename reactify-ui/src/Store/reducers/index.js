f70687e12ea4ac982622271c6709e8996ea858f8
Neeraj <neerajtanwar17@gmail.com>
2022-02-21 13:58:35 +0530

    version 1

--- reactify-ui/src/Store/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux'
-// import userData from './reducers'
-import {userData, notificationData} from "./reducers";
-
-export default combineReducers({
-    userData, notificationData
-})
