b9eb60a6fbea076a0960c62b275c1245541027b0
Neeraj <neerajtanwar17@gmail.com>
2022-02-23 11:22:15 +0530

    version 1

--- BlogApp/settings.py
+++ BlogApp/settings.py
@@ -92,7 +92,12 @@ ASGI_APPLICATION = 'BlogApp.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
