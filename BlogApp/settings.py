fb0b69a8dfae6e19f47c9aeb2d848824996352f3
neeraj <neerajsinghtanwar17@gmail.com>
2022-02-26 17:31:48 +0530

    first commit

--- BlogApp/settings.py
+++ BlogApp/settings.py
@@ -92,21 +92,21 @@ ASGI_APPLICATION = 'BlogApp.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'the_article_db',
-        'USER': 'postgresuser',
-        'PASSWORD': 'password123',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'the_article_db',
+#         'USER': 'postgresuser',
+#         'PASSWORD': 'password123',
+#         'HOST': 'localhost'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
