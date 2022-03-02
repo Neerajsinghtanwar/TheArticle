dfd0a813b8d42360780acb0f7ea046f97e046d8b
neeraj <neerajsinghtanwar17@gmail.com>
2022-03-02 21:02:20 +0530

    commit

--- BlogApp/settings.py
+++ BlogApp/settings.py
@@ -145,9 +145,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
