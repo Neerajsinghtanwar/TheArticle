f0c80f60ff9baa37226966c63fd3c8832e83e014
neeraj <neerajsinghtanwar17@gmail.com>
2022-03-02 17:51:42 +0530

    commit

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -31,7 +31,6 @@ urlpatterns = [
 
     # path('__debug__/', include('debug_toolbar.urls')),
 
-    path('index/', index, name='index'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
